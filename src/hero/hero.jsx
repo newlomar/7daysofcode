474be64cfa1a0bcb40977e32eef49253c8c3ed85
Newton Lomar <newtonlomar16@gmail.com>
2022-04-02 20:18:49 -0300

    navbar + hero styling

--- src/hero/hero.jsx
+++ src/hero/hero.jsx
@@ -1,8 +1,29 @@
+import "./hero.css";
+import image from "./imagem-hero1.svg";
+
 function Hero() {
   return (
-    <section>
-      <h1>As melhores plantas</h1>
-    </section>
+    <>
+      <div className="background"></div>
+      <section>
+        <div>
+          <p>Suas casas com as</p>
+          <h1>melhores plantas</h1>
+          <p>
+            Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
+            torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu
+            e-mail e assine nossa newsletter para saber das novidades da marca.
+          </p>
+          <form>
+            <input type="text" name="" id="" placeholder="Insira seu e-mail" />
+            <button>Assinar newsletter</button>
+          </form>
+        </div>
+        <div>
+          <img src={image} alt="plant" />
+        </div>
+      </section>
+    </>
   );
 }
 
--- src/menu/menu.jsx
+++ src/menu/menu.jsx
@@ -1,13 +1,25 @@
+import logo from "./logo.svg";
+import "./menu.css";
+
 function Menu() {
   return (
     <header>
-      <ul>
-        <li>Como fazer</li>
-        <li>Ofertas</li>
-        <li>Depoimentos</li>
-        <li>Vídeos</li>
-        <li>Meu carrinho</li>
-      </ul>
+      <div className="container row">
+        <img src={logo} alt="brand logo" />
+        <nav>
+          <ul>
+            <li>Como fazer</li>
+            <li>/</li>
+            <li>Ofertas</li>
+            <li>/</li>
+            <li>Depoimentos</li>
+            <li>/</li>
+            <li>Vídeos</li>
+            <li>/</li>
+            <li>Meu carrinho</li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
