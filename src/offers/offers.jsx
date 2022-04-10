00032f31b54c3a9e5300e56fafdaad21001a4169
Newton Lomar <newtonlomar16@gmail.com>
2022-04-10 18:18:53 -0300

    adding first offers markup + style

---
+++ src/offers/offers.jsx
@@ -0,0 +1,43 @@
+function Offers() {
+  const offers_list = [
+    {
+      url: "",
+      name: "Ajuga reptans",
+      price: "R$ 20,00",
+    },
+    {
+      url: "",
+      name: "Cordyline fruticosa",
+      price: "R$ 20,00",
+    },
+    {
+      url: "",
+      name: "Crassula ovata",
+      price: "R$ 20,00",
+    },
+    {
+      url: "",
+      name: "Cyperus rotundus",
+      price: "R$ 20,00",
+    },
+    {
+      url: "",
+      name: "Delairea odorata",
+      price: "R$ 20,00",
+    },
+    {
+      url: "",
+      name: "Datura metel",
+      price: "R$ 20,00",
+    },
+  ];
+  return (
+    <section>
+      <p>Conheça nossas</p>
+      <h3>Ofertas</h3>
+      <div>
+        <ul></ul>
+      </div>
+    </section>
+  );
+}
